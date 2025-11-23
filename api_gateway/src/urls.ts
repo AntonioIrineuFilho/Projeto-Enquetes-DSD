588831d43760eaf09362f924f2a2f8e15d954ce4
nicollasprado <nicollasprado.profissional@gmail.com>
2025-11-23 17:34:00 -0300

    feat: docker compose + api gateway dockerfile

--- api_gateway/src/urls.ts
+++ api_gateway/src/urls.ts
@@ -1,6 +1,9 @@
+const REST_HOST = process.env.NODE_ENV === "production" ? "rest" : "localhost";
+const SOAP_HOST = process.env.NODE_ENV === "production" ? "soap" : "localhost";
+
 const URLS = {
-  AUTH_REST_API: "http://localhost:3334",
-  ENQUETE_SOAP_API: "localhost:3335",
+  AUTH_REST_API: `http://${REST_HOST}:3334`,
+  ENQUETE_SOAP_API: `http://${SOAP_HOST}:3335`,
 };
 
 export default URLS;
